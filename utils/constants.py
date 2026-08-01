0107047652740361044862969d2d657bd9397398
Amna Fatima <amnafatimaa6@gmail.com>
2026-08-01 00:34:06 -0700

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# ===============================
+# Physical Constants (SI Units)
+# ===============================
+
+G = 6.67430e-11              # Gravitational Constant
+C = 299792458                # Speed of Light
+M_SUN = 1.98847e30           # Solar Mass (kg)
+
+# Astronomical Units
+AU = 1.495978707e11          # Astronomical Unit (m)
+PARSEC = 3.085677581e16      # Parsec (m)
+
+# LIGO
+DEFAULT_SAMPLE_RATE = 4096
+
+# GW150914
+GW150914_GPS = 1126259462.4
+
+PI = np.pi
